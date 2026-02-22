73f26ccdb2fa226a40b17e090dc4d1d3d4211b45
Santhosh-Charanthu <santhoshcharanthu455@gmail.com>
2026-02-22 14:52:45 +0530

    Added mobile notification

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -13,8 +13,8 @@
 const admin = require("firebase-admin");
 
 // Read the secret file created by Render
-// const serviceAccount = require("/etc/secrets/firebaseServiceKey.json");
-var serviceAccount = require("../config/firebaseServiceKey.json");
+const serviceAccount = require("/etc/secrets/firebaseServiceKey.json");
+// var serviceAccount = require("../config/firebaseServiceKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
